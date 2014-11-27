b3da6b0b91524888a78ede34858c822d717006e0
Marek Rusinowski <marekrusinowski@gmail.com>
2014-11-28 00:07:53 +0100

    feat: added file storage on server side

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const _ = require('lodash');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const LocalStorage = require('node-localstorage').LocalStorage;
+filesStorge = new LocalStorage('./files_storage');
 
 app.get('/', function (req, res) {
     res.sendFile('gui/main.html', { root: __dirname });
@@ -13,6 +16,59 @@ app.get('/vendor/*', function (req, res) {
 app.get(['/gui/*.js', '/gui/*.css', '/gui/*.html'], function (req, res) {
     res.sendFile(req.url, { root: __dirname });
 });
+
+app.use(bodyParser.json());
+
+var filesCache = prepareFilesCache();
+
+app.get('/file', function (req, res) {
+    res.send(filesCache);
+});
+
+app.get('/file/:fileId', function (req, res) {
+    var file = filesStorge.getItem(req.params.fileId);
+    if (file === null) {
+        res.status(404);
+        res.send("Not Found");
+        return;
+    }
+    var data = JSON.parse(file);
+    res.send(data.content);
+});
+
+app.post('/file', function (req, res) {
+    var data = req.body;
+    if (!data || !data.content || !data.name || !data.user) {
+        res.status(400);
+        res.send("Bad Request");
+        return;
+    }
+    data.id = crypto.randomBytes(20).toString('hex');
+    data.timestamp = new Date().getTime();
+    var meta = getMeta(data);
+    filesCache.push(meta);
+    res.send(meta);
+    filesStorge.setItem(data.id, JSON.stringify(data));
+});
+
+function getFileMeta(data) {
+    return {
+        id: data.id,
+        timestamp: data.timestamp,
+        user: data.user,
+        name: data.name
+    };
+}
+
+function prepareFilesCache() {
+    var i, content, filesCache = [];
+
+    for (i = 0; i < filesStorge.length; ++i) {
+        content = filesStorge.getItem(filesStorge.key(i));
+        filesCache.push(getMeta(JSON.parse(content)));
+    }
+    return filesCache;
+}
 
 const server = app.listen(process.env.PORT || 3000, process.env.HOST || "0.0.0.0", function () {
     const host = server.address().address;
